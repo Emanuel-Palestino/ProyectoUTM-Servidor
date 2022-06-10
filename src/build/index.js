1d8fa659e3300d9f0363c45619f2609cf2fd78f6
CorvoHyatt <corvohyatt@gmail.com>
2022-06-09 22:25:35 -0500

    CRUD de Proyectos y sus tablas derivadas

--- src/build/index.js
+++ src/build/index.js
@@ -17,6 +17,9 @@ const ArticulosRoutes_1 = __importDefault(require("./routes/ArticulosRoutes"));
 const ArchivoYArticuloRoutes_1 = __importDefault(require("./routes/ArchivoYArticuloRoutes"));
 const EventosRoutes_1 = __importDefault(require("./routes/EventosRoutes"));
 const ActividadesRoutes_1 = __importDefault(require("./routes/ActividadesRoutes"));
+const ProyectosRoutes_1 = __importDefault(require("./routes/ProyectosRoutes"));
+const ProfesorYProyectoRoutes_1 = __importDefault(require("./routes/ProfesorYProyectoRoutes"));
+const ExternosProyectoRoutes_1 = __importDefault(require("./routes/ExternosProyectoRoutes"));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
@@ -42,6 +45,9 @@ class Server {
         this.app.use('/api/archivoYArticulo', ArchivoYArticuloRoutes_1.default);
         this.app.use('/api/eventos', EventosRoutes_1.default);
         this.app.use('/api/actividades', ActividadesRoutes_1.default);
+        this.app.use('/api/proyectos', ProyectosRoutes_1.default);
+        this.app.use('/api/profesorYproyecto', ProfesorYProyectoRoutes_1.default);
+        this.app.use('/api/externosProyecto', ExternosProyectoRoutes_1.default);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
