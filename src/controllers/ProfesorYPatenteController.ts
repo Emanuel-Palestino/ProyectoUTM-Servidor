070ae8208b56f594516538e9a4ac6d9ab12711eb
Emanuel <emanuel.palestino.h@gmail.com>
2022-06-17 18:17:38 -0500

    [ProfesorYPatente] corrección

--- src/controllers/ProfesorYPatenteController.ts
+++ src/controllers/ProfesorYPatenteController.ts
@@ -4,12 +4,12 @@ class ProfesorYPatenteController
 {
 	public async list(req: Request, res: Response ): Promise<void>
 	{
-		const respuesta = await pool.query('SELECT * FROM ProfesorYPatente order by idPatente');
+		const respuesta = await pool.query('SELECT * FROM profesorYPatenteorder by idPatente');
 		res.json( respuesta );
 	}
 	public async listOne(req: Request, res: Response): Promise <void>{
 		const {idProfesor, idPatente, esInterno} = req.params;
-		const respuesta = await pool.query(`SELECT * FROM ProfesorYPatente WHERE idProfesor=${idProfesor} AND idPatente =${idPatente} AND esInterno=${esInterno}`); 
+		const respuesta = await pool.query(`SELECT * FROM profesorYPatenteWHERE idProfesor=${idProfesor} AND idPatente =${idPatente} AND esInterno=${esInterno}`); 
 		if(respuesta.length>0){
 			res.json(respuesta[0]);
 			return ;
@@ -17,18 +17,18 @@ class ProfesorYPatenteController
 		res.status(404).json({'mensaje': 'Patente no encontrada'});
 	}
 	public async create (req:Request, res:Response): Promise <void>{
-		const resp= await pool.query ("INSERT INTO ProfesorYPatente set ?", [req.body]);
+		const resp= await pool.query ("INSERT INTO profesorYPatenteset ?", [req.body]);
 		res.json(resp);
 	}
 	
 	public async delete (req:Request, res:Response): Promise <void>{
 		const {idProfesor, idPatente, esInterno} = req.params;
-		const resp= await pool.query (`DELETE FROM ProfesorYPatente WHERE idProfesor=${idProfesor} AND idPatente =${idPatente} AND esInterno=${esInterno}`);
+		const resp= await pool.query (`DELETE FROM profesorYPatenteWHERE idProfesor=${idProfesor} AND idPatente =${idPatente} AND esInterno=${esInterno}`);
 		res.json(resp);
 	}
 	public async actualizar(req: Request, res: Response): Promise<void> {
 		const {idProfesor, idPatente, esInterno} = req.params;
-		const resp = await pool.query(`UPDATE ProfesorYPatente set ? WHERE idProfesor=${idProfesor} AND idPatente =${idPatente} AND esInterno=${esInterno}`, req.body);
+		const resp = await pool.query(`UPDATE profesorYPatenteset ? WHERE idProfesor=${idProfesor} AND idPatente =${idPatente} AND esInterno=${esInterno}`, req.body);
 		res.json(resp);
 		}
 }
