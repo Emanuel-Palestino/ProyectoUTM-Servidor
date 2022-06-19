7b4b2fcf400965c4e67cf46d16873643eb001d6b
CorvoHyatt <corvohyatt@gmail.com>
2022-06-19 15:42:58 -0500

    Planes Corregidos

--- src/controllers/PlanesController.ts
+++ src/controllers/PlanesController.ts
@@ -4,34 +4,34 @@ class PlanControllers
 {
 	public async list(req: Request, res: Response ): Promise<void>
 	{
-		const respuesta = await pool.query('SELECT * FROM plan order by idPlan');
+		const respuesta = await pool.query('SELECT * FROM planes order by idPlan');
 		res.json( respuesta );
 	}
 	public async listOne(req: Request, res: Response): Promise <void>{
 		const {id} = req.params;
-		let consulta='SELECT * FROM plan WHERE idPlan = '+id;
+		let consulta='SELECT * FROM planes WHERE idPlan = '+id;
 		const respuesta = await pool.query(consulta); 
 		if(respuesta.length>0){
 			res.json(respuesta[0]);
 			return ;
 		}
-		res.status(404).json({'mensaje': 'Patente no encontrada'});
+		res.status(404).json({'mensaje': 'Plan no encontrado'});
 	}
 	public async create (req:Request, res:Response): Promise <void>{
 		console.log(req.body);
-		const resp= await pool.query ("INSERT INTO plan set ?", [req.body]);
+		const resp= await pool.query ("INSERT INTO planes set ?", [req.body]);
 		res.json(resp);
 	}
 	
 	public async delete (req:Request, res:Response): Promise <void>{
 		const {id} = req.params
-		const resp= await pool.query (`DELETE FROM plan WHERE idPlan = ${id}`);
+		const resp= await pool.query (`DELETE FROM planes WHERE idPlan = ${id}`);
 		res.json(resp);
 	}
 	public async actualizar(req: Request, res: Response): Promise<void> {
 		const { id } = req.params;
-		const resp = await pool.query("UPDATE plan set ? WHERE idPlan= ?", [req.body, id]);
+		const resp = await pool.query("UPDATE planes set ? WHERE idPlan= ?", [req.body, id]);
 		res.json(resp);
 		}
 }
-export const planControllers = new PlanControllers();
+export const planesController = new PlanControllers();
