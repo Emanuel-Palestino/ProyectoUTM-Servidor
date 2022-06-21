51b071b4403bb068a64c0660284b38cb3e2bd128
goitia <gpegg0001@gmail.com>
2022-06-21 12:01:36 -0500

    servicio listTesistasByProfesorByPeriodo2

--- src/build/controllers/TesistasController.js
+++ src/build/controllers/TesistasController.js
@@ -79,5 +79,30 @@ class TesistasController {
             res.json(resp);
         });
     }
+    listTesistasByProfesorByPeriodo2(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { idProfesor, fechaIni, fechaFin } = req.params;
+            let respNombres;
+            let aux2 = [];
+            const resp = yield database_1.default.query(`SELECT DISTINCT t.* FROM tesistas AS t INNER JOIN profesorYTesis AS pyt INNER JOIN profesores AS p WHERE pyt.idProfesor=${idProfesor} AND t.idTesis=pyt.idTesis AND t.inicio >= '${fechaIni}' and t.inicio <= '${fechaFin}'`);
+            for (var i = 0; i < resp.length; i++) {
+                const respColab = yield database_1.default.query(`SELECT idProfesor,esInterno,rol FROM profesorYTesis where profesorYTesis.idTesis=${resp[i].idTesis} ORDER BY pos ASC`);
+                console.log(respColab);
+                let aux = [];
+                for (var j = 0; j < respColab.length; j++) {
+                    if (respColab[j].esInterno == "0") {
+                        respNombres = yield database_1.default.query(`SELECT nombreCodirector FROM externocodirector WHERE idExternoCodirector = ${respColab[j].idProfesor};`);
+                    }
+                    else {
+                        respNombres = yield database_1.default.query(`SELECT nombreProfesor FROM profesores where profesores.idProfesor=${respColab[j].idProfesor}`);
+                    }
+                    aux.push(respNombres);
+                }
+                resp[i].nombreProfesor = aux;
+            }
+            console.log(aux2);
+            res.json(resp);
+        });
+    }
 }
 exports.tesistasController = new TesistasController();
--- src/keys.ts
+++ src/keys.ts
@@ -2,7 +2,7 @@ export default {
 	database: {
 		host: 'localhost',
 		user: 'root',
-		password: '',
-		database: 'invvestiga'
+		password: 'ggg',
+		database: 'investiga'
 	}
 }   
