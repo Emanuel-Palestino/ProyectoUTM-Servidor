7845bc6fd52168108dbf0e0185028a44e5da8aaa
Rugal <scaryskriller@gmail.com>
2022-06-23 17:28:52 -0500

    metodos asignados 22-06-2022

--- src/build/routes/PatentesRoutes.js
+++ src/build/routes/PatentesRoutes.js
@@ -17,6 +17,7 @@ class PatentesRoutes {
         this.router.get('/listColaboradoresInternosPatentes/:idProfesor', PatentesController_1.patentesController.listColaboradoresInternosPatentes);
         this.router.get('/listColaboradoresExternosPatentes/:idProfesor', PatentesController_1.patentesController.colaboradoresExternos);
         this.router.get('/listColaboradoresExternosExistentesSinColaboracionPatentes/:idProfesor', PatentesController_1.patentesController.listColaboradoresExternosExistentesSinColaboracionPatentes);
+        this.router.get('/listProfesoresByInstitutoSinColaboradoresInternosByPatente/:idInstituto/:idPatente', PatentesController_1.patentesController.listProfesoresByInstitutoSinColaboradoresInternosByPatente);
     }
 }
 const patentesRoutes = new PatentesRoutes();
